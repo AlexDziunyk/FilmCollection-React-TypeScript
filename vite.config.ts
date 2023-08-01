83a6f96a120fe5f30bd2b7566346fc27824061f0
AlexDziunyk <alexdziunyk@gmail.com>
2023-08-01 16:52:08 +0300

    vite fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-base: "/FilmCollection-React-TypeScript"
 export default defineConfig({
   plugins: [react()],
+  base: "/FilmCollection-React-TypeScript"
 })
