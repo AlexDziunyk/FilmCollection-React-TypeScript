4ab5e0ec25c09a2aba756575167a13d54f49af49
AlexDziunyk <alexdziunyk@gmail.com>
2023-08-06 01:08:41 +0300

    app_v2 routes fix2

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,20 +12,20 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className='navbar__container'>
-        <Link className='navbar__logo' to="/">IMGB</Link>
+        <Link className='navbar__logo' to="/FilmCollection-React-TypeScript">IMGB</Link>
           <ul className='navbar__sections'>
-              <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/popular">Popular</NavLink>
-              <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/top_rated">Top Rated</NavLink>
-              <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/upcoming">Upcoming</NavLink>
+              <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/FilmCollection-React-TypeScript/popular">Popular</NavLink>
+              <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/FilmCollection-React-TypeScript/top_rated">Top Rated</NavLink>
+              <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/FilmCollection-React-TypeScript/upcoming">Upcoming</NavLink>
           </ul>
           <div className="navbar__burger" onClick={() => setIsOpened(!isOpened)}>
             {isOpened ? 
             <>
               <RxCross1 size={25} />
               <ul className='navbar__menu'>
-                <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/popular">Popular</NavLink>
-                <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/top_rated">Top Rated</NavLink>
-                <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/upcoming">Upcoming</NavLink>
+                <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/FilmCollection-React-TypeScript/popular">Popular</NavLink>
+                <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/FilmCollection-React-TypeScript/top_rated">Top Rated</NavLink>
+                <NavLink className={({isActive}) => isActive ? "navlink active-link" : 'navlink'} to="/FilmCollection-React-TypeScript/upcoming">Upcoming</NavLink>
               </ul>
             </>
             : 
